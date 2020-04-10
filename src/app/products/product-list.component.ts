8bba31c0a274b93b1c73d061f45174259ab0eb25
Jacob Tollette <jaketollette@gmail.com>
2020-04-09 22:49:30 -0500

    Initial

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+import { EMPTY } from 'rxjs';
+
+import { ProductService } from './product.service';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+
+  products$ = this.productService.productsWithCategory$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  constructor(private productService: ProductService) { }
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
